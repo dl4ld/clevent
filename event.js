406172d72ec200dc2a14578162ba65ab8bc1f981
Reggie <cushingreggie@gmail.com>
2021-03-11 12:13:39 +0100

    switch to actor abstraction

--- event.js
+++ event.js
@@ -1,37 +1,23 @@
 const cmdArgs = require('command-line-args')
-const secureAmqp = require('../cllibsecureamqp')
 
 const cmdOptions = [
-	{ name: 'send', alias: 's', type: String},
-	{ name: 'config', alias: 'c', type: String}
+	{ name: 'config', alias: 'c', type: String},
+	{ name: 'debug', type: Boolean}
 ]
 const options = cmdArgs(cmdOptions)
+
+const Actor = (options.debug) ?  require('../cllibsecureamqp').Actor : require('secureamqp').Actor
+
 options.config = options.config || "./config"
 const config = require(options.config)
-const toAddress = options.send
 let domainToken = null
 
 async function main() {
-	await secureAmqp.init(config)
-	const myAddress = secureAmqp.getMyAddress()
+	const actor = new Actor(config)
+	await actor.boot()
+	const myAddress = actor.id()
 	console.log("Actor address: ", myAddress)
-	console.log("Actor keys: ", secureAmqp.keys())
-
-	// Sign my Id by domain
-	secureAmqp.callFunction(config.domain, ".f.signActorId", secureAmqp.id, null, function(res) {
-		const token = res.msg
-		const verify = secureAmqp.verifyToken(token)
-		const decoded = secureAmqp.decodeToken(res.msg)
-		if(verify) {
-			console.log("Received domain token: ", token)
-			domainToken = token
-			
-			secureAmqp.emitEvent("codeRed", "boolean", "true", token)
-			//secureAmqp.emitEvent("codeOrange", "boolean", "true", token)
-			//secureAmqp.emitEvent("codeYellow", "boolean", "true", token)
-
-		}
-	})
+	actor.broadcast("codeRed", "boolean", "true")
 }
 
 
